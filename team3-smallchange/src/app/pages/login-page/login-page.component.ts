d2bb3747bb3fd8cd5555539e3a8587d67a557c6a
Chaitanya Reddy <lvcr.dev@gmail.com>
2022-09-05 10:58:22 +0530

    login page

---
+++ team3-smallchange/src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,20 @@
+
+// Allows JavaScript to be compiled - otherwise use of TypeScript is enforced.
+// @ts-nocheck
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sc-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  public ngOnInit(): void {
+    /* This component requires some JavaScript functionality. Please enter it within this ngOnInit() function. */
+    const body = document.getElementsByTagName("BODY")[0];
+    body.style.padding = "0";
+    body.style.margin = "0";
+  }
+
+}
