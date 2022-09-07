44fc7a4b2e3fe8d7d3c6711dd6a5af4740d769bd
Sushmitha <sushmithacg.20@gmail.com>
2022-09-07 12:14:24 +0530

    routing update

--- team3-smallchange/src/app/app-routing.module.ts
+++ team3-smallchange/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent},
+  { path: 'portfolio', component: PortfolioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
