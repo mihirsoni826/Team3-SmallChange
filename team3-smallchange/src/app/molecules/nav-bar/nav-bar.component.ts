8a241758c1eee25b563b509747842fba70f62f2b
Chaitanya Reddy <lvcr.dev@gmail.com>
2022-09-11 16:50:42 +0530

    login page

--- team3-smallchange/src/app/molecules/nav-bar/nav-bar.component.ts
+++ team3-smallchange/src/app/molecules/nav-bar/nav-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 
+
 @Component({
   selector: 'sc-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -20,13 +21,15 @@ export class NavBarComponent implements OnInit {
     document.getElementById('activity').children[0].innerHTML = 'Activity';
     document.getElementById('trade').children[0].innerHTML = 'Trade';
     document.getElementById('preferences').children[0].innerHTML = 'Preferences';
-
-  @Input() dashboardActive: boolean = false;
+  }
+  @Input() 
+  dashboardActive: boolean = false;
   @Input() portfolioActive: boolean = false;
   @Input() tradeActive: boolean = false;
   @Input() historyActive: boolean = false;
 
   @Input() username: string = "John Doe";
+
 
 
   logout()
