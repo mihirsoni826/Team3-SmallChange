0a1a3f3318d0e61dbdbe5f8cb585237b307c5d22
Soni, Mihir <mihirsoni826@gmail.com>
2022-09-13 22:03:45 +0530

    Modified modal to support dynamic variables

--- team3-smallchange/src/app/molecules/model/model.component.ts
+++ team3-smallchange/src/app/molecules/model/model.component.ts
@@ -1,38 +1,40 @@
-import { Component, OnInit,Input , Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-model',
   templateUrl: './model.component.html',
-  styleUrls: ['./model.component.css']
+  styleUrls: ['./model.component.css'],
 })
 export class ModelComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  @Input()
-  display :string="";
-  @Output()
-  btnYesClick = new EventEmitter<string>;
-  @Output()
-  btnNoClick = new EventEmitter<string>;
-  @Input()
-  value: string="";
-  @Input()
-  form: any={};
-  @Input()
-  modalSuccess:boolean=false;
+  @Input() display: string = '';
+  @Input() value: string;
+  @Input() form: any = {};
+  @Input() modalSuccess: boolean = false;
+  @Input() tradeAction: string = ''; // sale or purchase
   
+  @Output() btnYesClick = new EventEmitter<string>();
+  @Output() btnNoClick = new EventEmitter<string>();
+
+  receivableOrPayable: string = '';
+  valueNumber: number;
+
   ngOnInit(): void {
+    this.valueNumber = +this.value;
+    
+    if (this.tradeAction === 'Sale') {
+      this.receivableOrPayable = 'Receivable';
+    } else if (this.tradeAction === 'Purchase') {
+      this.receivableOrPayable = 'Payable';
+    }
   }
 
-  onYesClick(eve :Event)
-  {
+  onYesClick(eve: Event) {
     eve.preventDefault();
     this.btnYesClick.emit();
-    
   }
-  onNoClick(eve :Event)
-  {
+  onNoClick(eve: Event) {
     eve.preventDefault();
     this.btnNoClick.emit();
   }
