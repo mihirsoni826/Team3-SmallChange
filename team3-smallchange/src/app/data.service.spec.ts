938214cabcab6557f89f87bf589bc6a9f4fe76bd
Rachana P Bennur <rachanapbennur@gmail.com>
2022-09-14 10:51:39 +0530

    added srvice spec file.

--- team3-smallchange/src/app/data.service.spec.ts
+++ team3-smallchange/src/app/data.service.spec.ts
@@ -1,28 +1,79 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule,HttpTestingController } from "@angular/common/http/testing";
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { DataService } from './data.service';
 
 describe('DataService', () => {
   let service: DataService;
   let httpTestingController: HttpTestingController;
+  var originalTimeout;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [DataService]
-    });
-
+      providers: [DataService],})
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(DataService);
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+    // jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it("should send data when 'getBrokeragePortfolio' is called",async() => { 
+  it("getBrokeragePortfolio() should return data", async() => {
+    const dummyBrokerageData = [
+      {
+        "id": 4,
+        "symbol": "GOOGL",
+        "tradeDate": "07-09-22",
+        "price": 100,
+        "quantity": 5,
+        "investedAmt": 500,
+        "presentValue": 550,
+        "PL": "50(10%)"
+      }
+    ]
     await service.getBrokeragePortfolio().subscribe((response) => {
-      expect(response.length).toBe(6);
+      expect(response.length).toBe(1);
+      expect(response).toEqual(dummyBrokerageData)
+     
     });
+
+  const httpRequest = httpTestingController.expectOne('../assets/brokerage-portfolio.json');
+  expect(httpRequest.request.method).toBe('GET');
+  httpRequest.flush(dummyBrokerageData);
   })
+
+
+it("getTradeHistory() should return data", async() => {
+  const dummyTradeHistoryData = [
+    {
+      "id": "5",
+     "name": "Dominos",
+     "acc": "savings",
+     "dop": "07-09-2022",
+     "dos": "-",
+     "bs": "Buy",
+     "asset": "XYZ",
+     "bp": "199",
+     "sp": "-",
+     "quantity": "10"
+    }
+  ]
+ await service.getTradeHistory().subscribe((response) => {
+    expect(response.length).toBe(1);
+    expect(response).toEqual(dummyTradeHistoryData)
+    
+  });
+  const httpRequest = httpTestingController.expectOne('../assets/tradehistory.json');
+  expect(httpRequest.request.method).toBe('GET');
+  httpRequest.flush(dummyTradeHistoryData);
+})
 });
+
