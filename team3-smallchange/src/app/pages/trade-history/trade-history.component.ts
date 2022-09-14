e5eb42a0dd80faca680a818f2047ba8b54cd2d7a
Rachana P Bennur <rachanapbennur@gmail.com>
2022-09-14 11:09:55 +0530

    changed asset class options.

--- team3-smallchange/src/app/pages/trade-history/trade-history.component.ts
+++ team3-smallchange/src/app/pages/trade-history/trade-history.component.ts
@@ -13,9 +13,9 @@ export class TradeHistoryComponent implements OnInit {
   selectedValue: string = 'Select Type of Account';
 
   OPTIONS1: string[] = ["Brokerage", "401K", 'IRAs', 'HSAs']
-  OPTIONS2: string[] = ["Asset1", "Asset2", "Asset3", "Asset4"]
+  OPTIONS2: string[] = ["Main Index Stocks", "Small Cap Company Stocks", "International Market Stocks", "Government Stocks", "Corporate Stocks"]
   OPTIONS3: string[] = ["Buy", "Sell"]
-  
+
   labelForA: string = 'account'
   labelForB: string = 'asset'
   labelForC: string = 'trade'
