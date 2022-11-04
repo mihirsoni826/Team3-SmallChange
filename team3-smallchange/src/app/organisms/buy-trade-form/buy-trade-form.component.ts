4c24052b67f0a88563d07ca9d438de16103cc7a9
Soni, Mihir <mihirsoni826@gmail.com>
2022-11-05 01:45:47 +0530

    Implemented all functionalities from buy side with hardcoded email id

--- team3-smallchange/src/app/organisms/buy-trade-form/buy-trade-form.component.ts
+++ team3-smallchange/src/app/organisms/buy-trade-form/buy-trade-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ModelComponent } from 'src/app/molecules/model/model.component'; 
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 @Component({
   selector: 'app-buy-trade-form',
@@ -12,40 +15,51 @@ export class BuyTradeFormComponent implements OnInit {
   @ViewChild('security', { static: true }) security: FormControl;
   @ViewChild('quantity', { static: true }) quantity: FormControl;
   @ViewChild('bankAccount', { static: true }) bankAccount: FormControl;
+  @ViewChild('readOnlySecurityName', {static: true}) readOnlySecurityName: FormControl;
 
-  price: number = 0;
+  price: number = 123;
   insufficientBalance: boolean = false;
   securityPrice: number = 0;
+  securityName: String = "";
   dis: string = '';
   success: boolean = false;
 
-  tickers: string[] = [];
+  tickers: String[] = [];
+  tickerDetailsMap: Map<String, any[]> = new Map<String, any[]>;
 
-  assetClassList: string[] = [
-    'Main index stocks',
-    'Small cap company stocks',
-    'International market stocks',
-    'Government bonds',
-    'Corporate bonds',
-  ];
+  bankAccountList: string[] = [];
+  accountBalanceMap: Map<string, number> = new Map<string, number>;
 
-  bankAccountList: string[] = [
-    'HDFC Bank (01234456789)',
-    'ICICI Bank (65437889243)',
-  ];
+  totalValueOfTransaction: number = 0;
+  accNumberWithoutBrackets: string = "";
 
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private http: HttpClient) {}
 
-  onSubmit(data: any): void {
-    console.log(this.buyForm);
-    console.log(data);
-    
+  ngOnInit(): void {
+    this.fetchTickersFromDB();
+    this.fetchBankAccountDetails();
   }
 
-  toggleModalDisplay() {
+  onSubmit(data: any): void {
+    console.log(data);
+  }
+
+  toggleModalDisplay(buyForm: any) {
+    let accWithBracket: string = buyForm.value.bankAccount.split("(")[1];
+    this.accNumberWithoutBrackets = accWithBracket.substring(0, accWithBracket.length - 1);
+    
+    this.totalValueOfTransaction = this.securityPrice * this.quantity.value;
+    
+    this.insufficientBalance = this.accountBalanceMap.get(this.accNumberWithoutBrackets) < this.totalValueOfTransaction;
+
+
+    if(this.insufficientBalance)
+      return;
+    
     this.dis = 'display: flex';
+    let mc: ModelComponent = new ModelComponent(null);
+    mc.setTotalValue(this.securityPrice, this.quantity.value);
   }
 
   toggleModalDisplayOnYes(buyForm: any) {
@@ -61,5 +75,45 @@ export class BuyTradeFormComponent implements OnInit {
     this.dis = '';
     this.success = false;
     buyForm.resetForm();
+  }
+
+  updateSecurityMarketPrice(security: any): void {
+    let tickerSelected: String = security.control.value;
+    let priceNameArr: any[] = this.tickerDetailsMap.get(tickerSelected);
+    if(priceNameArr == undefined) {
+      this.securityPrice = 0;
+      this.securityName = "";
+    }
+    else {
+      this.securityPrice = priceNameArr[0];
+      this.securityName = priceNameArr[1];
+    }
+  }
+
+  async fetchTickersFromDB() {
+    const securitiesUrl = 'http://localhost:8080/securities';
+    let dataSource = this.http.get(securitiesUrl);
+    let data = dataSource.subscribe(async (response: any) => {
+        for (let i = 0; i < response.length; i++) {
+          const security = response[i];
+          let tempArr: any[] = [security.marketPrice, security.securityName];
+          this.tickerDetailsMap.set(security.ticker, tempArr);
+          this.tickers.push(security.ticker);
+        }
+    })
+    this.tickerDetailsMap.set("", undefined);
+  }
+
+  async fetchBankAccountDetails() {
+    const bankDetailsUrl = "http://localhost:8080/bank-details";
+    let payload = {"email": "123@gmail.com"}
+    let dataSource = this.http.post(bankDetailsUrl, payload);
+    let data = dataSource.subscribe(async (response: any) => {
+      let accountNameNumber: string = response.bankName + " (" + response.accountNumber + ")";
+      
+      this.accountBalanceMap.set(response.accountNumber, response.balance);
+      
+      this.bankAccountList.push(accountNameNumber);
+    })
   }
 }
