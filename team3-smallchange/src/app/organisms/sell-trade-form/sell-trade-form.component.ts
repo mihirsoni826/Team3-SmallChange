d770068b15cacb875ae504f49e15bfbbcfcb7a80
Chaitanya Reddy <lvcr.dev@gmail.com>
2022-11-07 16:25:48 +0530

    sell fix

--- team3-smallchange/src/app/organisms/sell-trade-form/sell-trade-form.component.ts
+++ team3-smallchange/src/app/organisms/sell-trade-form/sell-trade-form.component.ts
@@ -97,6 +97,7 @@ export class SellTradeFormComponent {
   } 
 
   updateSecurityMarketPrice(security: any): void {
+     this.insufficientSecurity=false;
     let tickerSelected: String = security.control.value;
     let priceNameArr: any[] = this.tickerDetailsMap.get(tickerSelected);
     if(priceNameArr == undefined) {
